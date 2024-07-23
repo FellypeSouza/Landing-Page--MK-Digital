c74c538542a33ede9680c8af8e3dd17865e6748a
FellypeSouza <fellypedev23@gmail.com>
2024-07-23 13:04:12 -0300

    commit

--- scripts/script.js
+++ scripts/script.js
@@ -19,4 +19,41 @@ setInterval(()=>{
     }
 
     counter.innerHTML = counterMin.innerHTML + ":" + counterSec.innerHTML;
-},1000)
+},1000);
+
+function faqAccordion(value) {
+    const faq1 = document.getElementById("faq1");
+    let resp1 = document.getElementById("resp1");
+
+    const faq2 = document.getElementById("faq2");
+    let resp2 = document.getElementById("resp2");
+
+    const faq3 = document.getElementById("faq3");
+    let resp3 = document.getElementById("resp3");
+
+    if (value === '1') {
+        if (resp1.style.display === 'none' || resp1.style.display === '') {
+            resp1.style.display = 'block';
+        }
+        else{
+            resp1.style.display = 'none';
+        }
+    }
+    else if (value === '2') {
+        if (resp2.style.display === 'none' || resp2.style.display === '') {
+            resp2.style.display = 'block';
+        }
+        else{
+            resp2.style.display = 'none';
+        }
+    }
+    else if (value === '3') {
+        if (resp3.style.display === 'none' || resp3.style.display === '') {
+            resp3.style.display = 'block';
+        }
+        else{
+            resp3.style.display = 'none';
+        }
+    }
+    
+}
